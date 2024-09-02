4f66919fcddb8f68a7e2ffb0d89ed8f42b56013b
alstdu <lexi.dugo@gmail.com>
2024-09-01 23:26:32 -0700

    Render to do item

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../styles/TodoItem.sass';
+
+const TodoItem = ({ todo, onToggle, onDelete, onEdit }) => {
+  const dueDate = new Date(todo.dueDate);
+  const today = new Date();
+  const isOverdue = today > dueDate && !todo.completed;
+
+  return (
+    <div className={`todo-item ${todo.completed ? 'completed' : ''} ${isOverdue ? 'overdue' : ''}`}>
+      <input 
+        type="checkbox" 
+        checked={todo.completed} 
+        onChange={() => onToggle(todo.id)} 
+      />
+      <span>{todo.text}</span>
+      {todo.dueDate && <span className="due-date">{dueDate.toDateString()}</span>}
+      <button onClick={onEdit}>Edit</button>
+      <button onClick={() => onDelete(todo.id)}>Delete</button>
+    </div>
+  );
+};
+
+export default TodoItem;
